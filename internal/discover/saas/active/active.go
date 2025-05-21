448424aefc55d4c7b14333bab389bcda350a3092
payoub125 <philip@method.security>
2025-05-21 17:12:11 -0400

    Move SaaS Bruteforce to Webscan (#162)
    * moved saas bruteforce command
    
    * remove brute

---
+++ internal/discover/saas/active/active.go
@@ -0,0 +1,134 @@
+package saas
+
+import (
+	// Standard
+	"context"
+	"fmt"
+	"log"
+	"strings"
+
+	// Generated
+	common "github.com/Method-Security/webscan/generated/go/common"
+	discoversaasfern "github.com/Method-Security/webscan/generated/go/discover/saas"
+
+	// Internal
+	discoversaasactivehelpers "github.com/Method-Security/webscan/internal/discover/saas/active/helpers"
+	// Utils
+	request "github.com/Method-Security/webscan/utils/request"
+	requesthelpers "github.com/Method-Security/webscan/utils/request/helpers"
+)
+
+func createSendHTTPRequestConfig(baseURL, path string, config discoversaasfern.DiscoverSaasActiveConfig, browserbaseSecrets *common.BrowserbaseRequestSecrets) common.SendHttpRequestConfig {
+	request := common.HttpRequest{
+		BaseUrl: baseURL,
+		Path:    path,
+		Method:  common.HttpMethodGet,
+		Params:  &common.HttpRequestParams{},
+	}
+	return common.SendHttpRequestConfig{
+		Request:            &request,
+		MaxRedirects:       config.MaxRedirects,
+		Insecure:           config.Insecure,
+		Timeout:            config.Timeout,
+		RequestMethod:      config.RequestMethod,
+		HeadlessConfig:     config.HeadlessConfig,
+		BrowserbaseConfig:  config.BrowserbaseConfig,
+		BrowserbaseSecrets: browserbaseSecrets,
+	}
+}
+
+func LaunchDiscoverSaasActive(ctx context.Context, config discoversaasfern.DiscoverSaasActiveConfig, browserbaseSecrets *common.BrowserbaseRequestSecrets) (*discoversaasfern.DiscoverSaasActiveReport, error) {
+	// Initialize report
+	report := discoversaasfern.DiscoverSaasActiveReport{
+		Config: &config,
+	}
+	errors := []string{}
+
+	// Gather fingerprints
+	selectedSaasFingerprints, errs := discoversaasactivehelpers.SelectFingerprints(config.SaasFingerprints, config.SaasCompanies)
+	if len(errs) > 0 {
+		errors = append(errors, errs...)
+	}
+	selectedSsoFingerprints, errs := discoversaasactivehelpers.SelectFingerprints(config.SsoFingerprints, config.SsoCompanies)
+	if len(errs) > 0 {
+		errors = append(errors, errs...)
+	}
+	if len(selectedSaasFingerprints.Fingerprints) == 0 {
+		errors = append(errors, "no SaaS fingerprints found")
+		report.Errors = errors
+		return &report, nil
+	}
+	if len(selectedSsoFingerprints.Fingerprints) == 0 {
+		errors = append(errors, "no SSO fingerprints found")
+		report.Errors = errors
+		return &report, nil
+	}
+
+	// Process each organization
+	attempts := []*discoversaasfern.SaasActiveAttempt{}
+	for _, org := range config.Orgs {
+		attempt := &discoversaasfern.SaasActiveAttempt{Org: org}
+		companies := []*discoversaasfern.SaasActiveCompany{}
+
+		// Process each company
+		for company, fingerprint := range selectedSaasFingerprints.Fingerprints {
+			companyResult := &discoversaasfern.SaasActiveCompany{Company: company}
+			requests := []*discoversaasfern.SaasActiveRequest{}
+
+			// Process each domain slug
+			for _, domainSlug := range fingerprint.DomainSlugs {
+				// Determine the schemas to use for the request
+				schemas := []string{"https"}
+				if !config.HttpsOnly {
+					schemas = append(schemas, "http")
+				}
+
+				// Process each schema
+				for _, schema := range schemas {
+					log.Printf("Processing company: %s in org: %s with schema: %s", company, org, schema)
+					saasRequest := &discoversaasfern.SaasActiveRequest{}
+
+					// Construct the full URL
+					slug := strings.Replace(domainSlug, "INPUT_ORG", org, 1)
+					fullURL := fmt.Sprintf("%s://%s", schema, slug)
+
+					// Construct the full URL
+					baseURL, path, err := requesthelpers.SplitTargetURL(fullURL)
+					if err != nil {
+						errors = append(errors, fmt.Sprintf("invalid address %s: %v", fullURL, err))
+						continue
+					}
+
+					// Send the request
+					httpConfig := createSendHTTPRequestConfig(baseURL, path, config, browserbaseSecrets)
+					httpRequestResponse, err := request.SendRequest(ctx, httpConfig)
+					if err != nil {
+						errors = append(errors, fmt.Sprintf("failed to probe %s: %s", fullURL, err))
+						continue
+					}
+					saasRequest.Request = httpRequestResponse
+
+					// Analyze the request
+					redirectedPage := len(httpRequestResponse.Response.RedirectChain) > 1
+					finding := discoversaasactivehelpers.AnalyzeSaasRequest(ctx, saasRequest, fingerprint, selectedSsoFingerprints, redirectedPage)
+					saasRequest.Findings = finding
+
+					// Add request if it meets our criteria
+					if discoversaasactivehelpers.ShouldAddRequest(saasRequest, config.SuccessfulOnly) {
+						requests = append(requests, saasRequest)
+					}
+				}
+			}
+
+			companyResult.Requests = requests
+			companies = append(companies, companyResult)
+		}
+
+		attempt.Companies = companies
+		attempts = append(attempts, attempt)
+	}
+
+	report.Orgs = attempts
+	report.Errors = errors
+	return &report, nil
+}
---
+++ internal/discover/saas/active/helpers/analysis.go
@@ -0,0 +1,145 @@
+package saas
+
+import (
+	// Standard
+	"context"
+	"log"
+	"strings"
+
+	// Generated
+	discoversaasfern "github.com/Method-Security/webscan/generated/go/discover/saas"
+	// Utils
+	requesthelpers "github.com/Method-Security/webscan/utils/request/helpers"
+	// External
+	svc1log "github.com/palantir/witchcraft-go-logging/wlog/svclog/svc1log"
+)
+
+// AnalyzeSaasRequest analyzes a SaaS request and returns a finding
+func AnalyzeSaasRequest(ctx context.Context, request *discoversaasfern.SaasActiveRequest, saasFingerprint *discoversaasfern.SaasFingerprintEntry, selectedSsoFingerprints *discoversaasfern.SaasFingerprintFile, redirectedPage bool) *discoversaasfern.SaasActiveFinding {
+	log := svc1log.FromContext(ctx)
+	// Initial validation
+	// Note: If no response body or headers, or status code is not 200, return false
+	if (request.Request == nil && request.Request.Response == nil && request.Request.Response.ResponseHeaders == nil) ||
+		(request.Request == nil || request.Request.Response == nil || request.Request.Response.StatusCode == nil || *request.Request.Response.StatusCode != 200) {
+		return nil
+	}
+
+	if saasFingerprint.FingerprintProfile == nil {
+		log.Warn("No fingerprint profile found",
+			svc1log.SafeParam("baseUrl", request.Request.Request.BaseUrl),
+			svc1log.SafeParam("path", request.Request.Request.Path))
+		return nil
+	}
+
+	// Initialize values
+	companyPage := false
+	finding := &discoversaasfern.SaasActiveFinding{CompanyPage: &companyPage}
+
+	// Check for indicators that the webpage is not actually a valid SaaS page
+	// Note: Sometimes it will appear as a SaaS page but strings such as 'Not found' or '404' will be present
+	responseBodyStr := requesthelpers.GetResponseBodyStringFromBodyStruct(request.Request.Response.ResponseBody)
+	if isFalsePositive(responseBodyStr, &saasFingerprint.FingerprintProfile.PageNotFound) {
+		return finding
+	}
+
+	// Check for SSO Page first
+	// Note: SSO pages are only found if we have redirected to a new page from the intial SaaS page request
+	if redirectedPage {
+		for ssoCompany, ssoFingerprint := range selectedSsoFingerprints.Fingerprints {
+			if ssoFingerprint.FingerprintProfile == nil {
+				continue
+			}
+
+			if checkHeaders(ctx, request.Request.Response.ResponseHeaders, ssoFingerprint.FingerprintProfile.Headers) {
+				finding.SsoPage = &ssoCompany
+				break
+			}
+			// Check body if no header match
+			if checkBody(ctx, responseBodyStr, ssoFingerprint.FingerprintProfile.Body) {
+				finding.SsoPage = &ssoCompany
+				break
+			}
+		}
+	}
+
+	// Check for Company Page
+	if finding.SsoPage == nil {
+		if checkHeaders(ctx, request.Request.Response.ResponseHeaders, saasFingerprint.FingerprintProfile.Headers) {
+			companyPage = true
+			finding.CompanyPage = &companyPage
+		}
+		// Check for company match in body if not already found
+		if !companyPage && checkBody(ctx, responseBodyStr, saasFingerprint.FingerprintProfile.Body) {
+			companyPage = true
+			finding.CompanyPage = &companyPage
+		}
+	}
+
+	return finding
+}
+
+// isFalsePositive is a helper function to check for false positives
+func isFalsePositive(responseBody *string, notFoundPatterns *[]string) bool {
+	if responseBody == nil {
+		return false
+	}
+
+	if notFoundPatterns == nil {
+		log.Printf("[WARNING] No 'not found patterns' found for %s", *responseBody)
+		return false
+	}
+
+	bodyLower := strings.ToLower(*responseBody)
+	for _, pattern := range *notFoundPatterns {
+		if strings.Contains(bodyLower, strings.ToLower(pattern)) {
+			return true
+		}
+	}
+	return false
+}
+
+// checkHeaders is a helper function to check headers for matches
+func checkHeaders(ctx context.Context, headers map[string][]string, fingerprintHeaders map[string][]string) bool {
+	log := svc1log.FromContext(ctx)
+	if headers == nil || fingerprintHeaders == nil {
+		return false
+	}
+
+	for fingerprintHeader, fingerprintValue := range fingerprintHeaders {
+		// Normalize header key to lowercase for case-insensitive comparison
+		for headerKey, headerValue := range headers {
+			if strings.EqualFold(fingerprintHeader, headerKey) {
+				// If the fingerprint value is empty or matches (case insensitive)
+				if len(fingerprintValue) == 0 {
+					return true
+				}
+				for _, fingerprintValue := range fingerprintValue {
+					for _, headerValue := range headerValue {
+						if strings.EqualFold(fingerprintValue, headerValue) {
+							log.Debug("Header match found", svc1log.SafeParam("header", fingerprintHeader), svc1log.SafeParam("value", headerValue))
+							return true
+						}
+					}
+				}
+			}
+		}
+	}
+	return false
+}
+
+// checkBody is a helper function to check body for matches
+func checkBody(ctx context.Context, responseBody *string, fingerprintBody []string) bool {
+	log := svc1log.FromContext(ctx)
+	if responseBody == nil {
+		return false
+	}
+
+	bodyLower := strings.ToLower(*responseBody)
+	for _, bodyEntry := range fingerprintBody {
+		if strings.Contains(bodyLower, strings.ToLower(bodyEntry)) {
+			log.Debug("Body match found", svc1log.SafeParam("pattern", bodyEntry))
+			return true
+		}
+	}
+	return false
+}

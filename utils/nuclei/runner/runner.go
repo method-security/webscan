d3dfc6af230409808c4894c7e6ed35cac0692863
Philip Ayoub <philip@method.security>
2025-07-12 15:01:09 -0400

    comment fix

--- utils/nuclei/runner/runner.go
+++ utils/nuclei/runner/runner.go
@@ -197,7 +197,7 @@ func Run(ctx context.Context, cfg Config, reportBuilder *report.Builder) ([]*nuc
 	defer eng.Close()
 
 	// To-Do: Write Customer Writer to enable this to work
-	eng.Options().MatcherStatus = true
+	eng.Options().MatcherStatus = false
 	log.Info("Set matcher status", svc1log.SafeParam("status", eng.Options().MatcherStatus))
 
 	log.Info("Loading targets")

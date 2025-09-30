e02da38e9b0b7218b804d4a6b388af7b25311a8e
Philip Ayoub <philip@method.security>
2025-09-30 14:15:31 -0400

    updated engine

--- utils/nuclei/report/builder.go
+++ utils/nuclei/report/builder.go
@@ -4,6 +4,7 @@ import (
 	// Standard
 	"sync"
 	// Generated
+	common "github.com/Method-Security/webscan/generated/go/common"
 	nuclei "github.com/Method-Security/webscan/generated/go/common/nuclei"
 )
 
@@ -14,13 +15,17 @@ type Builder struct {
 	targets   []*nuclei.NucleiTargetInfo
 	probeIdx  map[string]*nuclei.NucleiProbe      // template-id → Probe
 	targetIdx map[string]*nuclei.NucleiTargetInfo // host/baseURL → TargetInfo
+
+	// Request collection for templates with multiple HTTP requests
+	pendingRequests map[string][]*common.HttpRequestResponse // template-id:target → requests
 }
 
 // NewBuilder creates and returns a new Builder instance.
 func NewBuilder() *Builder {
 	return &Builder{
-		targets:   []*nuclei.NucleiTargetInfo{},
-		probeIdx:  make(map[string]*nuclei.NucleiProbe),
-		targetIdx: make(map[string]*nuclei.NucleiTargetInfo),
+		targets:         []*nuclei.NucleiTargetInfo{},
+		probeIdx:        make(map[string]*nuclei.NucleiProbe),
+		targetIdx:       make(map[string]*nuclei.NucleiTargetInfo),
+		pendingRequests: make(map[string][]*common.HttpRequestResponse),
 	}
 }

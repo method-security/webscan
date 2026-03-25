3671b0deec1a25ba858d9d5b17f09b766c5729ec
Philip Ayoub <philip@method.security>
2026-03-25 13:17:46 -0400

    update (#352)

--- utils/request/headless/headless.go
+++ utils/request/headless/headless.go
@@ -63,10 +63,10 @@ func (b *Requester) InitializeBrowser(ctx context.Context) error {
 		binPath = resolved
 	}
 
-	launchCtx, launchCancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	launchCtx, launchCancel := context.WithTimeout(context.Background(), 10*time.Minute)
 	defer launchCancel()
 
-	launch := launcher.New().Headless(true).Bin(binPath).Context(launchCtx)
+	launch := launcher.New().Headless(true).Bin(binPath).NoSandbox(true).Context(launchCtx)
 	browserURL, err := launch.Launch()
 	if err != nil {
 		return fmt.Errorf("browser launch failed: %v", err)
@@ -99,7 +99,7 @@ func (b *Requester) SendRequest(ctx context.Context, config common.SendHttpReque
 	// Initialize browser if not already done
 	if b.Browser == nil {
 		if err := b.InitializeBrowser(ctx); err != nil {
-			return report, fmt.Errorf("browser initialization failed: %v", err)
+			return report, fmt.Errorf("browser initialization failed: %v", err) // Do not change, DD Metric is based on this error message
 		}
 
 		// Configure TLS verification

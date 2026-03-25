fa4de796a6411529faad500a2b0c6ea0cb75de4f
Philip Ayoub <philip@method.security>
2026-03-25 11:01:55 -0400

    fix cursor comments

--- utils/request/headless/headless.go
+++ utils/request/headless/headless.go
@@ -63,7 +63,10 @@ func (b *Requester) InitializeBrowser(ctx context.Context) error {
 		binPath = resolved
 	}
 
-	launch := launcher.New().Headless(true).Bin(binPath)
+	launchCtx, launchCancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer launchCancel()
+
+	launch := launcher.New().Headless(true).Bin(binPath).Context(launchCtx)
 	browserURL, err := launch.Launch()
 	if err != nil {
 		return fmt.Errorf("browser launch failed: %v", err)
